79bb28edbefdc3fda568c55bc862b4a7c5218c12
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-11-06 14:53:37 -0500

    Add song to page

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
+    http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("static/dist"))))
+    http.Handle("/music/", http.StripPrefix("/music/", http.FileServer(http.Dir("music"))))
+
     http.HandleFunc("/", indexHandler)
     http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("index.html"))
+    tmpl := template.Must(template.ParseFiles("index.tmpl.html"))
     err := tmpl.Execute(w, nil)
     if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
+
